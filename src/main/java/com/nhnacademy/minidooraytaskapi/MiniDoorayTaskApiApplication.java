ac2ea11c9ea29b69ed52239e455f2e28428ae115
suebin <suuebin@gmail.com>
2023-06-03 18:43:54 +0900

    Initial commit

---
+++ src/main/java/com/nhnacademy/minidooraytaskapi/MiniDoorayTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraytaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooraytaskapi/MiniDoorayTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraytaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
