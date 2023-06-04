8e82dec90d9bb9ad9459f0682bb1e7425c567399
권형택 <naht94@hanmail.net>
2023-06-04 14:20:45 +0900

    [naht94] modify task Dto

--- src/main/java/com/nhnacademy/minidooraytaskapi/task/dto/TaskDto.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/task/dto/TaskDto.java
@@ -1,4 +1,13 @@
 package com.nhnacademy.minidooraytaskapi.task.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskDto {
+    private Long taskId;
+    private Long taskWriterId;
+    private Long milestoneId;
+    private String title;
 }
--- src/main/java/com/nhnacademy/minidooraytaskapi/task/entity/Task.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/task/entity/Task.java
@@ -1,4 +1,10 @@
 package com.nhnacademy.minidooraytaskapi.task.entity;
 
 public class Task {
+    private Long taskId;
+    private Long projectId;
+    private Long mileStone;
+    private String title;
+    private String content;
+    private String taskWriterMemberId;
 }
