79b3b5d874f4f17b52ec2097fd029d545f11459d
권형택 <naht94@hanmail.net>
2023-06-04 19:40:26 +0900

    [naht94] modify testcode

--- src/main/java/com/nhnacademy/minidooraytaskapi/task/service/DefaultTaskService.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/task/service/DefaultTaskService.java
@@ -1,11 +1,21 @@
 package com.nhnacademy.minidooraytaskapi.task.service;
 
+import com.nhnacademy.minidooraytaskapi.task.entity.Task;
 import com.nhnacademy.minidooraytaskapi.task.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class DefaultTaskService {
+public class DefaultTaskService implements TaskService{
     private TaskRepository taskRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Task> getAllByProjectId(Long projectId) {
+        return taskRepository.getAllByProject_ProjectId(projectId);
+    }
 }
