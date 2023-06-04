fe226c4de4e31ffbabb91bbdb271a69e6bd6a9eb
권형택 <naht94@hanmail.net>
2023-06-04 14:48:47 +0900

    [naht94] modify task Dto type

--- src/main/java/com/nhnacademy/minidooraytaskapi/task/dto/TaskDto.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/task/dto/TaskDto.java
@@ -1,13 +1,8 @@
 package com.nhnacademy.minidooraytaskapi.task.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class TaskDto {
-    private Long taskId;
-    private Long taskWriterId;
-    private Long milestoneId;
-    private String title;
+public interface TaskDto {
+    Long getTaskId();
+    Long getTaskWriterId();
+    Long getMilestoneId();
+    String getTitle();
 }
