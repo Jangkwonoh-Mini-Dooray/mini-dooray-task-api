5ad57e0a85c21ac7c8c68d9222f45c4aa1a1d3ef
suebin <suuebin@gmail.com>
2023-06-04 14:51:38 +0900

    [suebin] modify project entity

--- src/main/java/com/nhnacademy/minidooraytaskapi/project/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/project/controller/ProjectController.java
@@ -21,5 +21,4 @@ public class ProjectController {
         return projectService.getProjects(memberId);
     }
 
-
 }
--- src/main/java/com/nhnacademy/minidooraytaskapi/project/repository/ProjectRepository.java
+++ src/main/java/com/nhnacademy/minidooraytaskapi/project/repository/ProjectRepository.java
@@ -4,6 +4,7 @@ package com.nhnacademy.minidooraytaskapi.project.repository;
 import com.nhnacademy.minidooraytaskapi.project.dto.ProjectDto;
 import com.nhnacademy.minidooraytaskapi.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
